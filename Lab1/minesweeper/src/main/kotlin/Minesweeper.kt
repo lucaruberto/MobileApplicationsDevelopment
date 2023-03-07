45fe94f489d8ec32c98468e2450210c0849fa62a
Andrea Torredimare <andrea@torredimare.it>
2023-03-07 15:56:02 +0100

    minesweeper ok
    mineswepeer passes all tests

--- Lab1/minesweeper/src/main/kotlin/Minesweeper.kt
+++ Lab1/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,18 +1,34 @@
 data class MinesweeperBoard constructor (val input: List<String>){
     fun withNumbers(): List<String> {
 
-        val returnBoard: List<String> = emptyList()
+        var returnBoard: List<String> = emptyList()
 
         if (input.isNotEmpty()) {
-            val rows = input.size
-            val columns = input[0].length
+            val maxRows = input.size
+            val maxColumns = input[0].length
 
             if(input[0] != ""){
-                for(i in 0 until rows){
-                    val line: String = input[i]
-                    for(j in 0 until columns){
-                        //TODO to be completed
+                for(i in 0 until maxRows){
+                    var line = ""
+                    for(j in 0 until maxColumns){
+                        if(input[i][j] == ' ') {
+                            var count = 0
+                            for(r_ind in i-1 .. i+1) {
+                                for (c_ind in j - 1 .. j + 1) {
+                                    if (r_ind < 0 || r_ind >= maxRows || c_ind < 0 || c_ind >= maxColumns)
+                                        continue
+                                    else
+                                        if (input[r_ind][c_ind] == '*')
+                                            count++
+                                }
+                            }
+                            if(count==0) line += ' '
+                            else line += count
+                        }
+                        else
+                            line += '*'
                     }
+                    returnBoard = returnBoard.plus(line)
                 }
                 return returnBoard
             } else
