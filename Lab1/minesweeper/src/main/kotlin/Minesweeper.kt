4d6b06040fcaf1fa2895b13c7ce907d749e8d401
andre296185 <s296185@studenti.polito.it>
2023-03-08 12:42:30 +0100

    edit in more efficient way

--- Lab1/minesweeper/src/main/kotlin/Minesweeper.kt
+++ Lab1/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -6,36 +6,29 @@ data class MinesweeperBoard constructor (val input: List<String>){
         if (input.isNotEmpty()) {
             val maxRows = input.size
             val maxColumns = input[0].length
-
-            if(input[0] != ""){
-                for(i in 0 until maxRows){
-                    var line = ""
-                    for(j in 0 until maxColumns){
-                        if(input[i][j] == ' ') {
+            for(row in 0 until maxRows){
+                var newLine = ""
+                if(input[row].isNotEmpty()) {
+                    for (col in 0 until maxColumns) {
+                        var newChar = '*'
+                        if (input[row][col] != '*') {
                             var count = 0
-                            for(r_ind in i-1 .. i+1) {
-                                for (c_ind in j - 1 .. j + 1) {
-                                    if (r_ind < 0 || r_ind >= maxRows || c_ind < 0 || c_ind >= maxColumns)
+                            for (r in row - 1..row + 1) {
+                                for (c in col - 1..col + 1) {
+                                    if (r < 0 || r >= maxRows || c < 0 || c >= maxColumns)
                                         continue
-                                    else
-                                        if (input[r_ind][c_ind] == '*')
-                                            count++
+                                    if (input[r][c] == '*')
+                                        count++
                                 }
                             }
-                            if(count==0) line += ' '
-                            else line += count
+                            newChar = if (count > 0) count.digitToChar() else ' '
                         }
-                        else
-                            line += '*'
+                        newLine = newLine.plus(newChar)
                     }
-                    returnBoard = returnBoard.plus(line)
                 }
-                return returnBoard
-            } else
-                return input
-
+                returnBoard = returnBoard.plus(newLine)
+            }
         }
-        else
-            return input
+        return returnBoard
     }
 }
