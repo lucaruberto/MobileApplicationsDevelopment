8344d94ee10e523927affa088ac7c450a8553883
andre296185 <s296185@studenti.polito.it>
2023-03-09 13:50:38 +0100

    user defined commands

--- Lab1/forth/src/main/kotlin/Forth.kt
+++ Lab1/forth/src/main/kotlin/Forth.kt
@@ -1,72 +1,140 @@
 import java.lang.Exception
-import java.util.*
-
 class Forth {
-    fun evaluate(vararg line: String): List<Int> {
-        val argList = line.toList()
-        val numbers = mutableListOf<Int>()
 
-        for(arg in argList){
-            for(a in arg.split(' ')) {
-                if (a.toIntOrNull() != null ) { //number
-                    numbers.add(a.toInt())
-                } else { //operator
-                    val operator = a.lowercase(Locale.getDefault())
-                    if(numbers.isEmpty())
-                        throw Exception("empty stack")
-                    if(numbers.size <= 1 && operator != "dup" && operator != "drop") //only dup and drop admit single value
-                        throw Exception("only one value on the stack")
-                    when (operator) {
-                        "+" -> {
-                            val n1 = numbers[numbers.size - 1]
-                            val n2 = numbers[numbers.size - 2]
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.add(0, n1+n2)
+    fun evaluate(vararg line: String): List<Int> {
+        val intList: MutableList<Int> = mutableListOf()
+        val userDefinedCommands: MutableMap<String,List<String>> = mutableMapOf()
+        for(rawCommand in line){
+            val command = rawCommand.lowercase()
+            if(command.startsWith(":")){
+                //user command definition
+                val cleanCommand = command.substring(2,command.length-2) //erase ": " and " ;"
+                val splitCommand = cleanCommand.split(" ").toMutableList()
+                if(splitCommand[0].toIntOrNull() != null){
+                    //redefining numbers
+                    throw Exception("illegal operation")
+                }
+                var i = 1   //ignore the first that is the name of the command
+                while (i < splitCommand.size){
+                    var c = splitCommand[i]
+                    while(userDefinedCommands.containsKey(c)){
+                        // translate user defined commands to base commands, to handle usage and redefinition
+                        splitCommand.addAll(i+1, userDefinedCommands.getValue(c))
+                        splitCommand.removeAt(i)
+                        c = splitCommand[i]
+                    }
+                    i++
+                }
+                userDefinedCommands[splitCommand[0]] = splitCommand.drop(1)
+            }
+            else {
+                val stringList = command.split(" ").toMutableList()
+                var i = 0
+                while (i < stringList.size) {
+                    val stringValue = stringList[i]
+                    val intValue = stringValue.toIntOrNull()
+                    if (intValue != null) {
+                        //string to int conversion successful
+                        intList.add(intValue)
+                    } else {
+                        //not an int, so is a command
+                        val size = intList.size
+                        if (userDefinedCommands.containsKey(stringValue)) {
+                            //command already present and must be applied
+                            stringList.addAll(i + 1, userDefinedCommands.getValue(stringValue))
                         }
-                        "-" -> {
-                            val n1 = numbers[numbers.size - 2]
-                            val n2 = numbers[numbers.size - 1]
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.add(0, n1-n2)
-                        }
-                        "*" -> {
-                            val n1 = numbers[numbers.size - 1]
-                            val n2 = numbers[numbers.size - 2]
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.add(0, n1*n2)
-                        }
-                        "/" -> {
-                            val n1 = numbers[numbers.size - 2]
-                            val n2 = numbers[numbers.size - 1]
-                            numbers.removeAt(numbers.size - 1)
-                            numbers.removeAt(numbers.size - 1)
-                            if(n2 == 0) throw Exception("divide by zero")
-                            else numbers.add(0, n1/n2)
-                        }
-                        "dup" -> {
-                            val numberToDup = numbers[numbers.size - 1]
-                            numbers.add(numberToDup)
-                        }
-                        "drop" -> {
-                            numbers.removeAt(numbers.size -1 )
-                        }
-                        "swap" -> {
-                            val tmp = numbers[numbers.size - 1]
-                            numbers[numbers.size - 1] = numbers[numbers.size - 2]
-                            numbers[numbers.size - 2] = tmp
-                        }
-                        "over" -> {
-                            val numberToCopy = numbers[numbers.size - 2]
-                            numbers.add(numberToCopy)
+                        else {
+                            when (stringValue) {
+                                "+" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    if (size == 1) {
+                                        throw Exception("only one value on the stack")
+                                    }
+                                    intList[size - 2] =
+                                        intList[size - 2] + intList[size - 1] //second-last + last
+                                    intList.removeLast() //remove last value
+                                }
+                                "-" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    if (size == 1) {
+                                        throw Exception("only one value on the stack")
+                                    }
+                                    intList[size - 2] =
+                                        intList[size - 2] - intList[size - 1] //second-last - last
+                                    intList.removeLast() //remove last value
+                                }
+                                "*" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    if (size == 1) {
+                                        throw Exception("only one value on the stack")
+                                    }
+                                    intList[size - 2] =
+                                        intList[size - 2] * intList[size - 1] //second-last * last
+                                    intList.removeLast() //remove last value
+                                }
+                                "/" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    if (size == 1) {
+                                        throw Exception("only one value on the stack")
+                                    } else if (intList.last() == 0) {
+                                        throw Exception("divide by zero")
+                                    }
+                                    intList[size - 2] =
+                                        intList[size - 2] / intList[size - 1] //second-last / last
+                                    intList.removeLast() //remove last value
+                                }
+                                "dup" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    intList.add(intList.last())
+                                }
+                                "drop" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    intList.removeLast()
+                                }
+                                "swap" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    if (size == 1) {
+                                        throw Exception("only one value on the stack")
+                                    }
+                                    val secondLast = intList[size - 2]
+                                    intList[size - 2] = intList[size - 1]
+                                    intList[size - 1] = secondLast
+                                }
+                                "over" -> {
+                                    if (size == 0) {
+                                        throw Exception("empty stack")
+                                    }
+                                    if (size == 1) {
+                                        throw Exception("only one value on the stack")
+                                    }
+                                    intList.add(intList[size - 2])
+                                }
+                                else -> {
+                                    //not user defined nor developer defined
+                                    throw Exception("undefined operation")
+                                }
+                            }
                         }
                     }
+                    i++
                 }
             }
         }
-
-        return numbers
+        return intList
     }
 }
+
