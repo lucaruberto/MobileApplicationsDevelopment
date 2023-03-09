37f21e6f9a1c537535f9e81dc35df156d0c1d1f3
andre296185 <s296185@studenti.polito.it>
2023-03-09 10:41:51 +0100

    ParsingAndNumberTest, Addition, Subtraction, Multiplication, Division, CombinedArithmetic

--- Lab1/forth/src/main/kotlin/Forth.kt
+++ Lab1/forth/src/main/kotlin/Forth.kt
@@ -1,9 +1,56 @@
 class Forth {
 
     fun evaluate(vararg line: String): List<Int> {
-        val final: = *line
-
-
-        return final
+        val intList: MutableList<Int> = mutableListOf()
+        for(command in line){
+            val stringList = command.split(" ")
+            for (stringValue in stringList){
+                val intValue = stringValue.toIntOrNull()
+                if(intValue != null){
+                    intList.add(intValue)
+                }
+                else{
+                    //command handler
+                    val size = intList.size
+                    if(size == 0) {
+                        throw Exception("empty stack")
+                    }
+                    when (stringValue) {
+                        "+" -> {
+                            if(size == 1) {
+                                throw Exception("only one value on the stack")
+                            }
+                            intList[size-2] = intList[size-2] + intList[size-1] //second-last + last
+                            intList.removeLast() //remove last value
+                        }
+                        "-" -> {
+                            if(size == 1) {
+                                throw Exception("only one value on the stack")
+                            }
+                            intList[size-2] = intList[size-2] - intList[size-1] //second-last - last
+                            intList.removeLast() //remove last value
+                        }
+                        "*" -> {
+                            if(size == 1) {
+                                throw Exception("only one value on the stack")
+                            }
+                            intList[size-2] = intList[size-2] * intList[size-1] //second-last * last
+                            intList.removeLast() //remove last value
+                        }
+                        "/" -> {
+                            if(size == 1) {
+                                throw Exception("only one value on the stack")
+                            }
+                            else if(intList.last() == 0) {
+                                throw Exception("divide by zero")
+                            }
+                            intList[size-2] = intList[size-2] / intList[size-1] //second-last / last
+                            intList.removeLast() //remove last value
+                        }
+                    }
+                }
+            }
+        }
+        return intList
     }
 }
