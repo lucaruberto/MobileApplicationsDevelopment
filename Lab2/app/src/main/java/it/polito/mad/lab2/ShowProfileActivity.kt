d0f482cdf8415f31b85eee98fffffabc317f3446
Andrea Torredimare <andrea@torredimare.it>
2023-03-21 13:43:21 +0100

    Pencil icon added on top right

--- Lab2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
+++ Lab2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
@@ -2,10 +2,16 @@ package it.polito.mad.lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
