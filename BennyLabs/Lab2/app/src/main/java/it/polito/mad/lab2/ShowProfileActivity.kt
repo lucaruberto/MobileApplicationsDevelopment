dfa96270270b8a47eff1dfd58188bd6fa60c1b61
FatBenny99 <benitomarra@gmail.com>
2023-03-29 01:27:50 +0200

    Layout Ended and EditProfileActivity Started

--- BennyLabs/Lab2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
+++ BennyLabs/Lab2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
@@ -1,11 +1,40 @@
 package it.polito.mad.lab2
 
+import android.content.ClipData.Item
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+
+
+    }
+
+    @Override
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.homemenu,menu);
+        return true;
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.pencil -> {
+                Toast.makeText(this, "Edit Mode!", Toast.LENGTH_SHORT).show();
+                val next_page=Intent(this,EditProfileActivity::class.java);
+                startActivity(next_page);
+                finish();
+                return  true;
+            }
+            else -> return  super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
