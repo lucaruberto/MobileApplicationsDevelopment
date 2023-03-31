8464c84734538936f3dd8e113947786e3f84ae21
FatBenny99 <benitomarra@gmail.com>
2023-03-31 02:18:16 +0200

    Edit mode Completed, missing Shared preferences and photo selection

--- BennyLabs/Lab2/app/src/main/java/it/polito/mad/lab2/EditProfileActivity.kt
+++ BennyLabs/Lab2/app/src/main/java/it/polito/mad/lab2/EditProfileActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -18,6 +21,7 @@ class EditProfileActivity : AppCompatActivity() {
     lateinit var sex : TextView;
     lateinit var city : TextView;
     lateinit var lists : TextView;
+    lateinit var profilepicture : ImageButton ;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +33,17 @@ class EditProfileActivity : AppCompatActivity() {
         sex=findViewById(R.id.EditSex);
         city=findViewById(R.id.EditCity);
         lists=findViewById(R.id.EditSportslist);
+        profilepicture=findViewById(R.id.EditImage);
+
+        nickname.text = this.intent?.getStringExtra("nickname");
+        fullname.text = this.intent?.getStringExtra("fullname");
+        email.text = this.intent?.getStringExtra("email");
+        birth.text = this.intent?.getStringExtra("birth");
+        sex.text = this.intent?.getStringExtra("sex");
+        city.text = this.intent?.getStringExtra("city");
+        lists.text = this.intent?.getStringExtra("lists");
+
+        registerForContextMenu(profilepicture);
 
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,10 +52,64 @@ class EditProfileActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+
+        menuInflater.inflate(R.menu.photocontextmenu,menu);
+        print("menu inflato");
+    }
+
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId)
+        {
+            R.id.Gallery-> {
+                Toast.makeText(this, "Select From gallery!", Toast.LENGTH_SHORT).show();
+                return  true;
+            }
+            R.id.Picture ->{
+                Toast.makeText(this, "Take a photo!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            }
+        }
+
+        return super.onContextItemSelected(item)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.check -> {
+                val i = Intent(this,ShowProfileActivity::class.java);
                 Toast.makeText(this, "Profile Mode!", Toast.LENGTH_SHORT).show();
+                if(nickname.text.toString()!= "")
+                {
+                    i.putExtra("nickname",nickname.text.toString());
+                }
+                if(fullname.text.toString()!= "")
+                {
+                    i.putExtra("fullname",fullname.text.toString());
+                }
+                if(email.text.toString()!= "")
+                {
+                    i.putExtra("email",email.text.toString());
+                }
+                if(birth.text.toString()!= "")
+                {
+                    i.putExtra("birth",birth.text.toString());
+                }
+                if(sex.text.toString()!= "")
+                {
+                    i.putExtra("sex",sex.text.toString());
+                }
+                if(city.text.toString()!= "")
+                {
+                    i.putExtra("city",city.text.toString());
+                }
+                if(lists.text.toString()!= "")
+                {
+                    i.putExtra("lists",lists.text.toString());
+                }
+                setResult(RESULT_OK,i);
                 finish();
                 return  true;
             }
--- BennyLabs/Lab2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
+++ BennyLabs/Lab2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
@@ -1,5 +1,6 @@
 package it.polito.mad.lab2
 
+import android.app.Activity
 import android.content.ClipData.Item
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,13 +8,46 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class ShowProfileActivity : AppCompatActivity() {
+
+    lateinit var nickname : TextView;
+    lateinit var fullname : TextView;
+    lateinit var email    : TextView;
+    lateinit var birth : TextView;
+    lateinit var sex : TextView;
+    lateinit var city : TextView;
+    lateinit var lists : TextView;
+
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            nickname.text = result.data?.getStringExtra("nickname");
+            fullname.text = result.data?.getStringExtra("fullname");
+            email.text = result.data?.getStringExtra("email");
+            birth.text = result.data?.getStringExtra("birth");
+            sex.text = result.data?.getStringExtra("sex");
+            city.text = result.data?.getStringExtra("city");
+            lists.text = result.data?.getStringExtra("lists");
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-
+        nickname=findViewById(R.id.NickName)
+        fullname=findViewById(R.id.FullName);
+        email=findViewById(R.id.Mail);
+        birth=findViewById(R.id.Birth);
+        sex=findViewById(R.id.Sex);
+        city=findViewById(R.id.City);
+        lists=findViewById(R.id.Sport);
 
     }
 
@@ -28,11 +62,64 @@ class ShowProfileActivity : AppCompatActivity() {
             R.id.pencil -> {
                 Toast.makeText(this, "Edit Mode!", Toast.LENGTH_SHORT).show();
                 val nextpage=Intent(this,EditProfileActivity::class.java);
-                startActivity(nextpage);
+                if(nickname.text.toString()!= "")
+                {
+                    nextpage.putExtra("nickname",nickname.text.toString());
+                }
+                if(fullname.text.toString()!= "")
+                {
+                    nextpage.putExtra("fullname",fullname.text.toString());
+                }
+                if(email.text.toString()!= "")
+                {
+                    nextpage.putExtra("email",email.text.toString());
+                }
+                if(birth.text.toString()!= "")
+                {
+                    nextpage.putExtra("birth",birth.text.toString());
+                }
+                if(sex.text.toString()!= "")
+                {
+                    nextpage.putExtra("sex",sex.text.toString());
+                }
+                if(city.text.toString()!= "")
+                {
+                    nextpage.putExtra("city",city.text.toString());
+                }
+                if(lists.text.toString()!= "")
+                {
+                    nextpage.putExtra("lists",lists.text.toString());
+                }
+
+
+                startForResult.launch(nextpage);
                 return  true;
             }
             else -> return  super.onOptionsItemSelected(item);
         }
         return super.onOptionsItemSelected(item);
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("nickname",nickname.text.toString());
+        outState.putString("fullname",fullname.text.toString());
+        outState.putString("email",email.text.toString());
+        outState.putString("birth",birth.text.toString());
+        outState.putString("sex",sex.text.toString());
+        outState.putString("city",city.text.toString());
+        outState.putString("lists",lists.text.toString());
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        nickname.text=savedInstanceState.getString("nickname");
+        fullname.text=savedInstanceState.getString("fullname");
+        email.text=savedInstanceState.getString("email");
+        birth.text=savedInstanceState.getString("birth");
+        sex.text=savedInstanceState.getString("sex");
+        city.text=savedInstanceState.getString("city");
+        lists.text=savedInstanceState.getString("lists");
+    }
+
 }
