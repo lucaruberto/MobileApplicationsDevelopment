a637cb7abb76c0aa278fd2858d885f30691a4fd4
FatBenny99 <benitomarra@gmail.com>
2023-04-26 22:57:40 +0200

    First Try using CustomCalendar

--- Lab3/app/src/main/java/it/polito/mad/lab2/SearchPlayground.kt
+++ Lab3/app/src/main/java/it/polito/mad/lab2/SearchPlayground.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.CalendarView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.stacktips.view.CalendarListener
+import com.stacktips.view.CustomCalendarView
+import java.util.Calendar
+import java.util.Locale
 import kotlin.math.log
 
 class SearchPlayground: Fragment(R.layout.fragment_search_playground) {
@@ -13,14 +19,18 @@ class SearchPlayground: Fragment(R.layout.fragment_search_playground) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        var  dropmenu : AutoCompleteTextView = view.findViewById(R.id.dropdown_sports)
-        val  db = GlobalDatabase.getDatabase(this.requireContext());
-        var listafinale :List<String> = listOf("");
-        val valore = db.sportsDao().getAll().observe(viewLifecycleOwner, Observer {
-            sports->dropmenu.setAdapter(ArrayAdapter(view.context,R.layout.list_item,sports));
+        var dropmenu: AutoCompleteTextView = view.findViewById(R.id.dropdown_sports)
+        val db = GlobalDatabase.getDatabase(this.requireContext());
+        val sportslist = db.sportsDao().getAll().observe(viewLifecycleOwner, Observer { sports ->
+            dropmenu.setAdapter(ArrayAdapter(view.context, R.layout.list_item, sports));
         })
 
-        
+        val calendarView = view.findViewById<CustomCalendarView>(R.id.playground_calendar_view)
+        var currentCalendar: Calendar = Calendar.getInstance(Locale.getDefault())
+        calendarView.firstDayOfWeek = Calendar.MONDAY
+        calendarView.setOnClickListener {
+            Toast.makeText(this.requireContext(),"Hai cliccato!",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
