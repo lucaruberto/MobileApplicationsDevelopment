18f04d1d03f0a0131ab7a7ec51407dc8a6914c2f
andre296185 <s296185@studenti.polito.it>
2023-04-27 17:50:43 +0200

    add portrait/landscape view with fragments of ShowReservation

---
+++ Lab3/app/src/main/java/it/polito/mad/lab2/CalendarView.kt
@@ -0,0 +1,6 @@
+package it.polito.mad.lab2
+
+import androidx.fragment.app.Fragment
+
+class CalendarView: Fragment(R.layout.fragment_calendar_view) {
+}
---
+++ Lab3/app/src/main/java/it/polito/mad/lab2/ReservationsList.kt
@@ -0,0 +1,6 @@
+package it.polito.mad.lab2
+
+import androidx.fragment.app.Fragment
+
+class ReservationsList: Fragment(R.layout.fragment_reservations_list) {
+}
