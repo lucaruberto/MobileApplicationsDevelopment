3e9735b2d8bcb07bf2e61e832e9ab1aa3161841e
FatBenny99 <benitomarra@gmail.com>
2023-04-30 19:08:46 +0200

    SearchPlayground ViewModel Implemented

--- Lab3/app/src/main/java/it/polito/mad/lab2/SearchPlaygroundViewModel.kt
+++ Lab3/app/src/main/java/it/polito/mad/lab2/SearchPlaygroundViewModel.kt
@@ -2,13 +2,41 @@ package it.polito.mad.lab2
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import it.polito.mad.lab2.db.FasciaOraria
 import it.polito.mad.lab2.db.GlobalDatabase
+import it.polito.mad.lab2.db.Reservation
+import java.util.Date
+import kotlin.concurrent.thread
 
 class SearchPlaygroundViewModel(application: Application) : AndroidViewModel(application) {
 
     val db = GlobalDatabase.getDatabase(getApplication<Application>().applicationContext);
 
+
+    fun getFasceOrari():LiveData<List<FasciaOraria>>{
+
+        return db.fasciaorariaDao().getAllFasciaOraria()
+    }
+    fun getListSport():LiveData<List<String>>{
+
+        return db.sportsDao().getAll()
+    }
+
+    fun getPlaygroundsbyName(sport:String):LiveData<List<String>>{
+        return db.playgroundsDao().getPlayGroundsbySportName(sport)
+    }
+
+    fun getRecyclerAdapter(fasce:List<FasciaOraria>,sport:String,field:String,date:Date):Playground_RecyclerViewAdapter{
+
+        return Playground_RecyclerViewAdapter(fasce.map { x->it.polito.mad.lab2.ShowReservationModel(sport,field,date,x.oraInizio,x.oraFine) },date,sport,field,this);
+    }
+
+    fun saveReservation(id: Int,date: Date,time:String,discipline:String,starthour:Int,endHour:Int,field: String){
+        thread { db.reservationDao().save(Reservation(id,date,time,discipline,starthour,endHour,field)) };
+        return
+    }
 
 }
