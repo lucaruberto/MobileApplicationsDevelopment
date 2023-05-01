ff0834de87d654598de73d0edc7d40fabd634c46
lucaruberto <lucaruberto@outlook.com>
2023-05-01 11:44:17 +0200

    Custom Request added

--- Lab3/app/src/main/java/it/polito/mad/lab2/db/GlobalDatabase.kt
+++ Lab3/app/src/main/java/it/polito/mad/lab2/db/GlobalDatabase.kt
@@ -2,8 +2,11 @@ package it.polito.mad.lab2.db
 
 import android.content.Context
 import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Reservation::class, Sports::class,PlayGrounds::class,FasciaOraria::class], version = 1)
+@Database(entities = [Reservation::class, Sports::class,PlayGrounds::class,FasciaOraria::class], version = 2)
 @TypeConverters(Converters::class)
 abstract class GlobalDatabase: RoomDatabase() {
     abstract  fun reservationDao(): ReservationDao
@@ -13,9 +16,15 @@ abstract class GlobalDatabase: RoomDatabase() {
 
     abstract fun fasciaorariaDao():FasciaOrariaDao
 
+
     companion object {
         @Volatile
         private var INSTANCE: GlobalDatabase? = null
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE reservations ADD COLUMN customRequest TEXT NOT NULL DEFAULT ''")
+            }
+        }
 
         fun getDatabase(context: Context): GlobalDatabase =
             (INSTANCE ?:
@@ -25,6 +34,7 @@ abstract class GlobalDatabase: RoomDatabase() {
                     GlobalDatabase::class.java,
                     "reservation_database").
                 createFromAsset("db/reservation.db").
+                addMigrations(MIGRATION_1_2).
                 build()
                 INSTANCE = i
                 INSTANCE
