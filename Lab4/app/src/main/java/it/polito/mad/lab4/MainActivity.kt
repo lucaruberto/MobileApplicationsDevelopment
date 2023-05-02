a61b39076b74ab08dd74c573225d77fe3ed138b4
lucaruberto <lucaruberto@outlook.com>
2023-05-02 17:16:13 +0200

    Few changes

--- Lab4/app/src/main/java/it/polito/mad/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/it/polito/mad/lab4/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.DateRange
 import androidx.compose.material.icons.sharp.Person
 import androidx.compose.material.icons.sharp.Place
 import androidx.compose.material.icons.sharp.Search
@@ -29,6 +30,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -60,22 +62,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-
+@Preview(showBackground = true)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Mainscreen(){
     val navController = rememberNavController()
     Scaffold(
         bottomBar = {
-            BottomAppBar(Modifier.background(MaterialTheme.colorScheme.primary) ){
+            BottomAppBar( ){
 
                Row(Modifier.fillMaxWidth()){
                    Column( modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, ) {
-                       Button(onClick = { navController.navigate("ScreenOne") }) {
+                       Button(onClick = { navController.navigate("ScreenOne") }, colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondaryContainer)) {
                            Icon(
                                Icons.Sharp.Person,
                                contentDescription = "Profile",
-                               modifier = Modifier.size(ButtonDefaults.IconSize)
+                               modifier = Modifier.size(ButtonDefaults.IconSize),
+                               tint = Color.Black
                            )
                        }
                        Text("Profile", color = MaterialTheme.colorScheme.primary)
@@ -83,11 +86,12 @@ fun Mainscreen(){
                    }
 
                     Column( modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                        Button(onClick = { navController.navigate("ScreenTwo") }, ) {
+                        Button(onClick = { navController.navigate("ScreenTwo") }, colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondaryContainer)  ) {
                             Icon(
-                                Icons.Sharp.Place,
+                                Icons.Sharp.DateRange,
                                 contentDescription = "Calendar",
-                                modifier = Modifier.size(ButtonDefaults.IconSize)
+                                modifier = Modifier.size(ButtonDefaults.IconSize),
+                                tint = Color.Black
                             )
                         }
                         Text("Reservations", color = MaterialTheme.colorScheme.primary)
@@ -95,22 +99,24 @@ fun Mainscreen(){
                     }
 
                    Column( modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                        Button(onClick = { navController.navigate("ScreenThree") }) {
+                        Button(onClick = { navController.navigate("ScreenThree") }, colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondaryContainer)) {
                             Icon(
                                 Icons.Sharp.Search,
                                 contentDescription = "Rent",
-                                modifier = Modifier.size(ButtonDefaults.IconSize)
+                                modifier = Modifier.size(ButtonDefaults.IconSize),
+                                tint = Color.Black
                             )
                         }
                         Text("Rent", color = MaterialTheme.colorScheme.primary)
 
                     }
                    Column( modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                       Button(onClick = { navController.navigate("ScreenFour") }) {
+                       Button(onClick = { navController.navigate("ScreenFour") }, colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondaryContainer)) {
                            Icon(
                                Icons.Sharp.Star,
                                contentDescription = "Rate",
-                               modifier = Modifier.size(ButtonDefaults.IconSize)
+                               modifier = Modifier.size(ButtonDefaults.IconSize),
+                               tint = Color.Black
                            )
                        }
                        Text("Rent", color = MaterialTheme.colorScheme.primary)
@@ -165,7 +171,7 @@ fun Rate(){
             style= MaterialTheme.typography.headlineLarge)
     }
 }
-@Preview(showBackground = true)
+
 @Composable
 fun GreetingPreview() {
     Lab4Theme {
