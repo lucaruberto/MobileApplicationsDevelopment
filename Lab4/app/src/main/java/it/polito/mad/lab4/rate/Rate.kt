a08349c961e8a18c479b7dfa4e5d2161cf391915
Andrea Torredimare <andrea@torredimare.it>
2023-05-15 12:45:40 +0200

    Add: load reviews and propagate delete from/to db

--- Lab4/app/src/main/java/it/polito/mad/lab4/rate/Rate.kt
+++ Lab4/app/src/main/java/it/polito/mad/lab4/rate/Rate.kt
@@ -13,11 +13,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -26,57 +27,56 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import it.polito.mad.lab4.db.Rating
 
 
 @Composable
 fun Rate() {
+    val vm: RateViewModel = viewModel()
+    val ratings by vm.getAllReviews().observeAsState()
+
     Column(modifier = Modifier.padding(24.dp)) {
-        ReviewComponent(text = "Buongiorno")
-        Spacer(modifier = Modifier.height(16.dp))
-        ReviewComponent(text = "Prova")
+        ratings?.forEach {
+            ReviewComponent(it.id, it.reviewText, it.fieldName, vm)
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 }
 
 @Composable
-fun ReviewComponent(text: String) {
+fun ReviewComponent(reviewId: Int, fieldName: String, reviewText: String, vm: RateViewModel) {
     val (showButtons, setShowButtons) = remember { mutableStateOf(false) }
 
-    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp).background(Color(0xFFF5F5F5))) {
-        // Text Field
+    Column(modifier = Modifier
+        .padding(start = 16.dp, end = 16.dp)
+        .background(Color(0xFFF5F5F5))) {
         Text(
-            text = text,
+            text = fieldName,
             style = TextStyle(fontSize = 24.sp),
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentWidth(align = Alignment.CenterHorizontally)
                 .clickable { setShowButtons(!showButtons) }
-
         )
-
         // Row with two buttons
         if (showButtons) {
+            Text(
+                text = reviewText,
+                style = TextStyle(fontSize = 20.sp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.CenterHorizontally)
+            )
             Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
+                horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
                 IconButton(
-                    onClick = { /* do something */ },
-                    modifier = Modifier
-                        .background(Color(0xFFFFF59D))
-                        .size(48.dp),
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Edit,
-                        contentDescription = "Edit",
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-
-                IconButton(
-                    onClick = { /* do something */ },
+                    onClick = { vm.deleteReview( Rating(reviewId, reviewText, fieldName )) },
                     modifier = Modifier
                         .background(Color(0xFFFFCDD2))
                         .size(48.dp),
