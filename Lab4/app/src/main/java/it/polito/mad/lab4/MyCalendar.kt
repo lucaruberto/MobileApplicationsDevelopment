d45eb2a0911202cf2432ee7995da727f754efbee
andre296185 <s296185@studenti.polito.it>
2023-05-15 11:16:40 +0200

    add reservations calendar

---
+++ Lab4/app/src/main/java/it/polito/mad/lab4/MyCalendar.kt
@@ -0,0 +1,168 @@
+package it.polito.mad.lab4
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.OutDateStyle
+import com.kizitonwose.calendar.core.daysOfWeek
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
+
+@Composable
+fun MyCalendar(selectedDate: LocalDate?, setSelectedDate: (LocalDate?)->Unit, isColored: (CalendarDay)->Boolean ) {
+
+    val currentMonth = remember { YearMonth.now() }
+    val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
+    val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
+
+    val daysOfWeek = remember { daysOfWeek() }
+
+    val state = rememberCalendarState(
+        startMonth = startMonth,
+        endMonth = endMonth,
+        firstVisibleMonth = currentMonth,
+        firstDayOfWeek = daysOfWeek.first(),
+        outDateStyle = OutDateStyle.EndOfGrid
+    )
+    HorizontalCalendar(
+        state = state,
+        dayContent = { day ->
+            if(isColored(day)){
+
+            }
+            Day(day,
+                isSelected = selectedDate == day.date,
+                isColored = isColored(day),
+                onClick =  { day ->
+                setSelectedDate(day.date)
+            })
+        },
+        monthHeader = {
+                month -> MonthHeader(calendarMonth = month, daysOfWeek = daysOfWeek)
+        },
+        // Draw the day content gradient.
+        monthBody = { _, content ->
+            Box(
+                modifier = Modifier.background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFFB2EBF2),
+                            Color(0xFFB2B8F2)
+                        )
+                    )
+                )
+            ) {
+                content() // Render the provided content!
+            }
+        },
+        // Add the corners/borders and month width.
+        monthContainer = { _, container ->
+            val configuration = LocalConfiguration.current
+            val screenWidth = configuration.screenWidthDp.dp
+            Box(
+                modifier = Modifier
+                    .width(screenWidth /* * 0.73f*/)
+                    .padding(8.dp)
+                    .clip(shape = RoundedCornerShape(8.dp))
+                    .border(
+                        color = Color.Black,
+                        width = 1.dp,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+            ) {
+                container() // Render the provided container!
+            }
+        }
+    )
+
+}
+
+@Composable
+private fun MonthHeader(calendarMonth: CalendarMonth, daysOfWeek: List<DayOfWeek>) {
+    Column(
+        modifier = Modifier
+            .wrapContentHeight()
+            .background(color = Color.Red)
+            .padding(top = 6.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = calendarMonth.yearMonth.format(DateTimeFormatter.ofPattern("yyyy MMMM")).toString(),
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            for (dayOfWeek in daysOfWeek) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.Center,
+                    fontSize = 15.sp,
+                    text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+                    fontWeight = FontWeight.Medium,
+                )
+            }
+        }
+        Divider(color = Color.Black)
+    }
+}
+
+@Composable
+fun Day(day: CalendarDay, isSelected: Boolean, isColored: Boolean, onClick: (CalendarDay) -> Unit) {
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .clip(CircleShape)
+            .background(color = if (isSelected) Color.Green else if(isColored) Color.Gray else Color.Transparent)
+            .clickable(
+                enabled = day.position == DayPosition.MonthDate,
+                onClick = { onClick(day) }
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = day.date.dayOfMonth.toString(),
+            color = if (day.position == DayPosition.MonthDate) Color.White else Color.Gray
+        )
+    }
+}
--- Lab4/app/src/main/java/it/polito/mad/lab4/reservation/Reservation.kt
+++ Lab4/app/src/main/java/it/polito/mad/lab4/reservation/Reservation.kt
@@ -7,21 +7,65 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.himanshoe.kalendar.Kalendar
+import androidx.lifecycle.viewmodel.compose.viewModel
+import it.polito.mad.lab4.MyCalendar
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toJavaInstant
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.util.Date
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun Reservation() {
-    //val vm: RentViewModel = viewModel()
-    Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
-        Text(text = "Your reservations", fontSize = 30.sp, fontStyle = FontStyle.Normal)
-        //Kalendar(kalendarType = KalendarType.Firey, modifier = Modifier.fillMaxWidth())
+    val vm: ShowReservationsViewModel = viewModel()
+    val liveDates by vm.getLiveDates().observeAsState(initial = emptyList())
+    val (selectedDate, setSelectedDate) = remember { mutableStateOf<LocalDate?>(null) }
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        /*.padding(16.dp)*/) {
+        Text(
+            text = "Your reservations",
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Normal,
+            modifier = Modifier.padding(16.dp)
+        )
+        MyCalendar(selectedDate, setSelectedDate) {
+            var found = false
+            for (d in liveDates) {
+                if (d == Date.from(it.date.atStartOfDay(ZoneOffset.systemDefault()).toInstant())) {
+                    found = true
+                    break
+                }
+            }
+            found
+        }
+        /*Kalendar(
+            kalendarType = KalendarType.Firey,
+            currentDay = Clock.System.todayIn(TimeZone.currentSystemDefault()),
+            modifier = Modifier.fillMaxWidth(),
+            kalendarDayKonfig = KalendarDayKonfig(
+                        size = 56.dp,
+                        textSize = 16.sp,
+                        textColor = Color.Green,
+                        selectedTextColor = Color.Black
+                    )
+
+        )*/
 
         Card {
             //vm.getListSport().value
