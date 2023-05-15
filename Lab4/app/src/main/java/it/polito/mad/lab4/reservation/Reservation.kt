0eaf46a48249ca661b196cca5377880b28f902c6
andre296185 <s296185@studenti.polito.it>
2023-05-15 17:54:00 +0200

    add reservations cards and edit MyCalendar appearance

--- Lab4/app/src/main/java/it/polito/mad/lab4/reservation/Reservation.kt
+++ Lab4/app/src/main/java/it/polito/mad/lab4/reservation/Reservation.kt
@@ -1,10 +1,23 @@
 package it.polito.mad.lab4.reservation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,13 +25,17 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import it.polito.mad.lab4.MyCalendar
+import it.polito.mad.lab4.R
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.toJavaInstant
@@ -33,26 +50,34 @@ import java.util.Date
 fun Reservation() {
     val vm: ShowReservationsViewModel = viewModel()
     val liveDates by vm.getLiveDates().observeAsState(initial = emptyList())
-    val (selectedDate, setSelectedDate) = remember { mutableStateOf<LocalDate?>(null) }
+    val (selectedDate, setSelectedDate) = remember { mutableStateOf<LocalDate?>(LocalDate.now()) }
 
-    Column(modifier = Modifier
+    val slots by vm.getReservationFromDate(Date.from(
+        selectedDate?.atStartOfDay(ZoneOffset.systemDefault())?.toInstant()
+    )).observeAsState(initial = emptyList())
+
+    LazyColumn(modifier = Modifier
         .fillMaxWidth()
         /*.padding(16.dp)*/) {
-        Text(
-            text = "Your reservations",
-            fontSize = 30.sp,
-            fontStyle = FontStyle.Normal,
-            modifier = Modifier.padding(16.dp)
-        )
-        MyCalendar(selectedDate, setSelectedDate) {
-            var found = false
-            for (d in liveDates) {
-                if (d == Date.from(it.date.atStartOfDay(ZoneOffset.systemDefault()).toInstant())) {
-                    found = true
-                    break
+        item{
+            Text(
+                text = "Your reservations",
+                fontSize = 30.sp,
+                fontStyle = FontStyle.Normal,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+        item{
+            MyCalendar(selectedDate, setSelectedDate, {
+                var found = false
+                for (d in liveDates) {
+                    if (d == Date.from(it.date.atStartOfDay(ZoneOffset.systemDefault()).toInstant())) {
+                        found = true
+                        break
+                    }
                 }
-            }
-            found
+                found
+            }, Color.Gray)
         }
         /*Kalendar(
             kalendarType = KalendarType.Firey,
@@ -66,10 +91,71 @@ fun Reservation() {
                     )
 
         )*/
+        items(items = slots.sortedBy { it.oraInizio }, key = { it.oraInizio}){
+            //Text(text = "${it.oraInizio}:00 - ${it.oraFine}:00")
+            Card(
+                shape = RoundedCornerShape(12. dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                ) {
+                Row(modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column() {
+                        Text(
+                            text = "${it.oraInizio}:00 - ${it.oraFine}:00",
+                            fontSize = 20.sp,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                        Text(
+                            text = "${it.discipline} at ${it.playgroundName}",
+                            fontSize = 16.sp,
+                            modifier = Modifier.padding(
+                                start = 16.dp,
+                                end = 16.dp,
+                                top = 0.dp,
+                                bottom = 16.dp)
+                        )
+                        if(it.customRequest != "") {
+                            Text(
+                                text = "Custom requests: ${it.customRequest}",
+                                fontSize = 16.sp,
+                                modifier = Modifier.padding(
+                                    start = 16.dp,
+                                    end = 16.dp,
+                                    top = 0.dp,
+                                    bottom = 16.dp)
+                            )
+                        }
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.End,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Button(onClick = { vm.deleteReservation(
+                            it.id,
+                            it.date,
+                            it.time,
+                            it.discipline,
+                            it.oraInizio,
+                            it.oraFine,
+                            it.playgroundName,
+                            it.customRequest) }) {
 
-        Card {
-            //vm.getListSport().value
+                            Icon(Icons.Rounded.Delete, contentDescription = "Delete Reservation")
+
+                        }
+                    }
+                }
+
+            }
         }
+        /*Card {
+            //vm.getListSport().value
+        }*/
     }
 
 }
