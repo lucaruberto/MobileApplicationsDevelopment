03feed768ad1cb980b88cc86899427dffa95de5e
Andrea Torredimare <andrea@torredimare.it>
2023-05-15 17:54:29 +0200

    Fully functional review system

--- Lab4/app/src/main/java/it/polito/mad/lab4/rate/Rate.kt
+++ Lab4/app/src/main/java/it/polito/mad/lab4/rate/Rate.kt
@@ -1,5 +1,6 @@
 package it.polito.mad.lab4.rate
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,20 +12,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -36,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -50,16 +49,27 @@ fun Rate() {
     val ratings by vmRatings.getAllReviews().observeAsState()
     val fieldsNotRated by vmRatings.getFieldsNotRated().observeAsState()
     val (readMode, setReadMode) = remember { mutableStateOf(true) }
+    val (showForm, setShowForm) = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+    var selectedField by remember { mutableStateOf("Select field") }
 
 
-    LazyColumn() {
-        if(readMode){
-            item {
-                ratings?.forEach {
-                    ReviewComponent(it.id, it.fieldName, it.reviewText, vmRatings, modifier = Modifier.padding(24.dp))
-                    Spacer(modifier = Modifier.height(16.dp))
+
+    if(readMode) {
+        LazyColumn {
+            ratings?.forEach {
+                item {
+                    ReviewComponent(
+                        it.id,
+                        it.fieldName,
+                        it.reviewText,
+                        vmRatings,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    //Spacer(modifier = Modifier.height(16.dp))
                 }
             }
+
             item {
                 Row(
                     horizontalArrangement = Arrangement.Center,
@@ -71,124 +81,121 @@ fun Rate() {
                 }
             }
         }
-        else {
-            item {
-                val (showForm, setShowForm) = remember { mutableStateOf(false) }
-                var expanded by remember { mutableStateOf(false) }
-                var selectedField by remember { mutableStateOf("") }
-                
-                Column {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                            .clickable(onClick = { expanded = true })
-                            .background(Color.LightGray),
-                        contentAlignment = Alignment.CenterStart
-                    ) {
-                        Text(selectedField, modifier = Modifier.padding(16.dp))
-                        Icon(
-                            imageVector = Icons.Default.ArrowDropDown,
-                            contentDescription = "Dropdown Icon",
-                            modifier = Modifier.padding(16.dp)
-                        )
-                    }
-                    DropdownMenu(
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        fieldsNotRated?.forEach { field ->
-                            DropdownMenuItem(
-                                text = { Text(text = field, modifier = Modifier.fillMaxWidth()) },
-                                onClick = {
-                                    selectedField = field
-                                    expanded = false
-                                },
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                        }
-                    }
-                }
-
-
-                InsertReviewForm(modifier = Modifier.padding(24.dp))
-                Row(
-                    horizontalArrangement = Arrangement.Center,
+    }
+    else {
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable(onClick = { expanded = true })
+                    .background(Color.LightGray),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    selectedField, modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.Center)
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Dropdown Icon",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                fieldsNotRated?.forEach { field -> DropdownMenuItem(
+                    text = { Text(text = field, modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally))},
+                    onClick = {
+                        selectedField = field
+                        expanded = false
+                        setShowForm(true) },
                     modifier = Modifier.fillMaxWidth()
-                ) {
-                    Button(
-                        onClick = { setReadMode(true) },
-                    ) {
-                        Text("Back")
-                    }
+                    )
+                }
+            }
+
+            if (showForm)
+                InsertReviewForm(modifier = Modifier.padding(24.dp), selectedField, vmRatings) { setReadMode(true) }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(onClick = {
+                    setReadMode(true)
+                    setShowForm(false)
+                }) {
+                    Text("Back")
                 }
             }
         }
     }
 }
+
+
 
 @Composable
 fun ReviewComponent(reviewId: Int, fieldName: String, reviewText: String, vm: RateViewModel, modifier: Modifier) {
     val (showButtons, setShowButtons) = remember { mutableStateOf(false) }
 
-    LazyColumn(modifier = modifier.then(
-        Modifier
-            .padding(start = 16.dp, end = 16.dp)
-            .background(Color(0xFFF5F5F5)))) {
-        item {
+    Column(
+        modifier = modifier.then(
+            Modifier
+                .padding(start = 16.dp, end = 16.dp)
+                .background(Color(0xFFF5F5F5))
+        )
+    ) {
+        Text(
+            text = fieldName,
+            style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center),
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+                .clickable { setShowButtons(!showButtons) }
+        )
+
+        if (showButtons) {
             Text(
-                text = fieldName,
-                style = TextStyle(fontSize = 24.sp, textAlign = TextAlign.Center),
+                text = reviewText,
+                style = TextStyle(fontSize = 20.sp, textAlign = TextAlign.Center),
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentWidth(align = Alignment.CenterHorizontally)
-                    .clickable { setShowButtons(!showButtons) }
+                    .padding(16.dp)
             )
-        }
-        // Row with two buttons
-        if (showButtons) {
-            item {
-                Text(
-                    text = reviewText,
-                    style = TextStyle(fontSize = 20.sp, textAlign = TextAlign.Center),
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                IconButton(
+                    onClick = { vm.deleteReview(Rating(reviewId, reviewText, fieldName)) },
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(align = Alignment.CenterHorizontally)
-                        .padding(16.dp)
-                )
-            }
-            item {
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
+                        .background(Color(0xFFFFCDD2))
+                        .size(48.dp),
                 ) {
-                    IconButton(
-                        onClick = { vm.deleteReview(Rating(reviewId, reviewText, fieldName)) },
-                        modifier = Modifier
-                            .background(Color(0xFFFFCDD2))
-                            .size(48.dp),
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Delete",
-                            modifier = Modifier.size(24.dp)
-                        )
-                    }
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete",
+                        modifier = Modifier.size(24.dp)
+                    )
                 }
             }
         }
     }
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InsertReviewForm(modifier: Modifier/*onSaveClicked: (String) -> Unit, onCancelClicked: () -> Unit*/) {
-    var text by remember { mutableStateOf("") }
+fun InsertReviewForm(modifier: Modifier, selectedField: String, vm: RateViewModel, onButtonClick: (Boolean) -> Unit) {
+    val context = LocalContext.current
+    var content by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier.then(
@@ -201,9 +208,9 @@ fun InsertReviewForm(modifier: Modifier/*onSaveClicked: (String) -> Unit, onCanc
        Spacer(modifier = Modifier.height(16.dp))
 
        TextField(
-           value = text,
-           onValueChange = { text = it },
-           label = { Text("Text Input") },
+           value = content,
+           onValueChange = { content = it },
+           label = { Text("Review...") },
            modifier = Modifier.fillMaxWidth()
        )
        Spacer(modifier = Modifier.height(16.dp))
@@ -211,7 +218,11 @@ fun InsertReviewForm(modifier: Modifier/*onSaveClicked: (String) -> Unit, onCanc
            horizontalArrangement = Arrangement.Center,
            modifier = Modifier.fillMaxWidth()
        ) {
-           Button(onClick = { /*onSaveClicked(text)*/ }) {
+           Button(onClick = {
+               vm.saveReview( Rating(0,content,selectedField) )
+               Toast.makeText(context, "Review saved", Toast.LENGTH_LONG).show()
+               onButtonClick(true)
+           }) {
                Text("Save")
            }
        }
