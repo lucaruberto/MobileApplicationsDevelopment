4b4c3c034ed7c0eb0541f40426a5252676c09d26
andre296185 <s296185@studenti.polito.it>
2023-05-27 10:13:41 +0200

    add update list of values, listener after login

--- ClassExamples/FireBaseTest/app/src/main/java/com/example/firebasetest/MainActivity.kt
+++ ClassExamples/FireBaseTest/app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.firebasetest
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.layout.rememberLazyNearestItemsRangeState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,11 +25,13 @@ import com.example.firebasetest.ui.MyAuthenticationViewModel
 import com.example.firebasetest.ui.theme.FireBaseTestTheme
 
 class MainActivity : ComponentActivity() {
+
+    val vm = FireBaseViewModel()
+    val authVm = MyAuthenticationViewModel { vm.startListenerRegistration() }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val vm = FireBaseViewModel()
-        val authVm = MyAuthenticationViewModel()
 
         setContent {
             FireBaseTestTheme {
@@ -44,22 +48,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyFireBase(vm: FireBaseViewModel) {
-    val users = vm.users
+    val text = vm.text.value
+    val texts = vm.texts
 
     LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            TextField(value = text, onValueChange = { vm.text.value = it}, modifier = Modifier.padding(16.dp))
+        }
         item {
             Button(modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp), onClick = {
-                vm.addUsers()
+                vm.addText()
                 //vm.readUsers()
             }) {
-                Text(text = "Add User")
+                Text(text = "Send Text")
             }
         }
-        items(users){
+        items(texts){
             Text(modifier = Modifier.padding(16.dp), text = it)
         }
     }
--- ClassExamples/FireBaseTest/app/src/main/java/com/example/firebasetest/ui/MyAuthenticationViewModel.kt
+++ ClassExamples/FireBaseTest/app/src/main/java/com/example/firebasetest/ui/MyAuthenticationViewModel.kt
@@ -1,25 +1,19 @@
 package com.example.firebasetest.ui
 
 
-import android.app.Activity
 import android.content.ContentValues
-import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.snapshots.SnapshotApplyResult
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
-class MyAuthenticationViewModel: ViewModel() {
+class MyAuthenticationViewModel(startListenerRegistration: () -> Unit) : ViewModel() {
     val auth = Firebase.auth
-
+    val startListenerRegistration = startListenerRegistration
     val email = mutableStateOf("")
     val password = mutableStateOf("")
     val repeatPassword = mutableStateOf("")
@@ -47,9 +41,13 @@ class MyAuthenticationViewModel: ViewModel() {
         viewModelScope.launch {
             if (email.value != "" && password.value != "") {
                 try {
-                    auth.signInWithEmailAndPassword(email.value, password.value).await()
-                    Log.d(ContentValues.TAG, "User logged in")
-                    isLogged.value = true
+                    val authResult = auth.signInWithEmailAndPassword(email.value, password.value).await()
+                    if(authResult.user != null){
+                        Log.d(ContentValues.TAG, "User logged in as " + authResult.user!!.uid)
+                        startListenerRegistration()
+                        isLogged.value = true
+                    }
+
                 } catch (e: Exception) {
                     Log.w(ContentValues.TAG, "Error user login", e)
                 }
